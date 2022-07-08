8b2979a4f23ba88e07427bd67d6ae037ab4865f3
Shamaun <shamaunalam@gmail.com>
2022-07-08 13:08:26 +0530

    second commit : test complete

--- tweeapp/urls.py
+++ tweeapp/urls.py
@@ -4,4 +4,6 @@ urlpatterns = [
     path('home',views.home,name='home'),
     path('gettweet',views.get_tweets,name='tweets'),
     path('',views.index, name='index'),
+    path('reg',views.signup,name='signup'),
+    path('logout',views.signout,name='logout')
 ]
--- tweeapp/views.py
+++ tweeapp/views.py
@@ -1,10 +1,23 @@
 from django.shortcuts import redirect, render
 from django.http import JsonResponse
 from . import analyze
+from django.contrib import messages
+
+
+### importing the django User class
+from django.contrib.auth.models import User
+
+### import django authentication and login/logout function
+from django.contrib.auth import authenticate,login,logout
+
+### 
+from django.contrib.auth.decorators import login_required
 # Create your views here.
 
 from .twitterapi import BEARER_TOKEN , search_twitter
 
+
+@login_required(login_url='index')
 def home(request):
 
     if request.method == 'POST':
@@ -16,7 +29,7 @@ def home(request):
 
         return render(request,'home.html')
 
-
+@login_required(login_url='index')
 def get_tweets(request):
 
     if request.method=='POST':
@@ -26,9 +39,6 @@ def get_tweets(request):
         content = dict()
         if not len(query)==0:
             json_response = search_twitter(query=query, tweet_fields=tweet_fields, bearer_token=BEARER_TOKEN)
-
-            
-
             if json_response['meta']['result_count']>0:
                 for i,t in enumerate(json_response['data']):
                     content.update({i:{'tweet':t['text'],'sent':analyze.get_absolute_sentiment(t['text'])}})
@@ -41,4 +51,35 @@ def get_tweets(request):
         return redirect('home')
 
 def index(request):
-    return render(request,'index.html')
+    if request.user.is_authenticated:
+        return redirect('home')
+    else:
+        if request.method=='POST':
+            username = request.POST['username']
+            pwd      = request.POST['password']
+            user = authenticate(username=username,password=pwd)
+            if user is not None:
+                login(request,user)
+                return redirect('home')
+            else:
+                messages.error(request,"Invalid Credentials")
+        return render(request,'index.html')
+
+def signup(request):
+
+    if request.method == 'POST':
+        username = request.POST['username']
+        name     = request.POST['name']
+        pwd      = request.POST['password']
+
+        user = User.objects.create(username=username,first_name=name)
+        user.set_password(pwd)
+        user.save()
+        messages.success(request,"User Created successfully")
+        return redirect('index')
+    else:
+        return redirect('index')
+
+def signout(request):
+    logout(request)
+    return redirect('index')
